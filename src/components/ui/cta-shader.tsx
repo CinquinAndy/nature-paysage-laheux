f69f43c4f662e072081958e5a2bad9b97d63be7f
CinquinAndy <cinquin.andy@gmail.com>
2025-10-10 17:13:20 +0200

    feat: cta postion

--- src/components/ui/cta-shader.tsx
+++ src/components/ui/cta-shader.tsx
@@ -37,28 +37,33 @@ export function CtaShader({
 			<EnhancedShaderBackground variant={shaderVariant} colors={shaderColors} />
 
 			{/* Content */}
-			<div className="relative z-10 container mx-auto">
-				<div className="flex justify-center">
+			<div className="relative z-10 container mx-auto ">
+				<div className="flex justify-center ">
 					<div className="max-w-5xl">
-						<div className="flex flex-col items-start justify-between gap-8 rounded-lg bg-background backdrop-blur-md border border-white/20 px-6 py-10 md:flex-row lg:px-20 lg:py-16 shadow-2xl">
-							<div className="md:w-1/2">
+						<div
+							className="flex flex-col items-start justify-between gap-8 rounded-lg bg-background backdrop-blur-md
+						 border border-white/20 px-6 py-10 md:flex-row lg:px-20 lg:py-16 shadow-2xl h-full"
+						>
+							<div className="md:w-1/2 h-full">
 								<h4 className="mb-4 text-3xl sm:text-4xl md:text-5xl font-bold text-foreground">{title}</h4>
 								<p className="text-foreground/90 text-lg mb-6">{description}</p>
-								<Button className="bg-white hover:bg-white/90 text-primary font-semibold " size="lg" asChild>
-									<Link href={buttonUrl}>
-										{buttonText} <ArrowRight className="ml-2 size-4" />
-									</Link>
-								</Button>
 							</div>
-							<div className="md:w-1/3">
+							<div className="md:w-1/3 h-full flex flex-col justify-between ">
 								<ul className="flex flex-col space-y-3 text-sm font-medium">
 									{items.map(item => (
 										<li className="flex items-center text-foreground" key={item}>
-											<Check className="mr-4 size-5 flex-shrink-0 text-green-300" />
+											<Check className="mr-4 size-5 flex-shrink-0 text-foreground" />
 											{item}
 										</li>
 									))}
 								</ul>
+								<div className="flex flex-col w-full h-full justify-end">
+									<Button className="bg-white hover:bg-white/90 text-primary font-semibold " size="lg" asChild>
+										<Link href={buttonUrl}>
+											{buttonText} <ArrowRight className="ml-2 size-4" />
+										</Link>
+									</Button>
+								</div>
 							</div>
 						</div>
 					</div>
